8b86b27fea4273c00e0d05e3508f92db0edd29ab
Yuki Iwanaga <yuki@creasty.com>
2018-01-29 23:43:18 +0900

    Split inspector of node

--- lib/i18n_flow/node.rb
+++ lib/i18n_flow/node.rb
@@ -1,6 +1,9 @@
 require 'psych'
+require_relative 'node_inspector'
 
 class I18nFlow::Node
+  include I18nFlow::NodeInspector
+
   TAG_TODO   = '!todo'
   TAG_IGNORE = '!ignore'
   TAG_ONLY   = /^!only:([\w-]+)$/
@@ -44,21 +47,6 @@ class I18nFlow::Node
     @tag == :ignore
   end
 
-  def inspect
-    [].tap do |lines|
-      lines << [
-        inspect_anchor,
-        scope.join('.'),
-        inspect_line,
-        inspect_tag,
-      ].compact.join(' ')
-
-      children.each do |c|
-        lines << c.inspect.gsub(/^/, "    ")
-      end
-    end.join("\n")
-  end
-
 private
 
   def parse_tag!(tag)
@@ -71,30 +59,6 @@ private
       @only = $1
     when TAG_IGNORE
       @tag = :ignore
-    end
-  end
-
-  def inspect_anchor
-    return unless has_anchor?
-    '&%s' % [@anchor]
-  end
-
-  def inspect_line
-    if end_line
-      '%d-%d (%d)' % [start_line, end_line, num_lines]
-    else
-      '%d' % [start_line]
-    end
-  end
-
-  def inspect_tag
-    case @tag
-    when :todo
-      'TODO'
-    when :ignore
-      'IGNORED'
-    when :only
-      'ONLY[%s]' % [@only]
     end
   end
 end
