1dadf89160269be84d8410373b10a96c58216e14
Yuki Iwanaga <yuki@creasty.com>
2018-01-29 23:34:17 +0900

    Parse tag

--- lib/i18n_flow/node.rb
+++ lib/i18n_flow/node.rb
@@ -1,15 +1,22 @@
 require 'psych'
 
 class I18nFlow::Node
+  TAG_TODO   = '!todo'
+  TAG_IGNORE = '!ignore'
+  TAG_ONLY   = /^!only:([\w-]+)$/
+
   attr_accessor :start_line
   attr_accessor :end_line
-  attr_accessor :scope
+  attr_reader :scope
+  attr_reader :only
 
   def initialize(o, scope:)
     @start_line = o.start_line + 1
-    @end_line = o.end_line || @start_line
+    @end_line = o.end_line
     @anchor = o.anchor
     @scope = scope
+
+    parse_tag!(o.tag)
   end
 
   def has_anchor?
@@ -17,6 +24,7 @@ class I18nFlow::Node
   end
 
   def num_lines
+    return 1 unless end_line
     end_line - start_line + 1
   end
 
@@ -28,18 +36,65 @@ class I18nFlow::Node
     @children ||= []
   end
 
+  def todo?
+    @tag == :todo
+  end
+
+  def ignored?
+    @tag == :ignore
+  end
+
   def inspect
     [].tap do |lines|
       lines << [
-        @anchor ? "&#{@anchor}" : nil,
+        inspect_anchor,
         scope.join('.'),
-        '%d-%d' % [@start_line, @end_line],
-        '[%d, %d]' % [level, num_lines],
+        inspect_line,
+        inspect_tag,
       ].compact.join(' ')
 
       children.each do |c|
         lines << c.inspect.gsub(/^/, "    ")
       end
     end.join("\n")
+  end
+
+private
+
+  def parse_tag!(tag)
+    return unless tag
+    case tag
+    when TAG_TODO
+      @tag = :todo
+    when TAG_ONLY
+      @tag = :only
+      @only = $1
+    when TAG_IGNORE
+      @tag = :ignore
+    end
+  end
+
+  def inspect_anchor
+    return unless has_anchor?
+    '&%s' % [@anchor]
+  end
+
+  def inspect_line
+    if end_line
+      '%d-%d (%d)' % [start_line, end_line, num_lines]
+    else
+      '%d' % [start_line]
+    end
+  end
+
+  def inspect_tag
+    case @tag
+    when :todo
+      'TODO'
+    when :ignore
+      'IGNORED'
+    when :only
+      'ONLY[%s]' % [@only]
+    end
   end
 end
