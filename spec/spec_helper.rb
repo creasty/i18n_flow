e77ec43b10165dbd76ea486b228e7fb8cf48a513
Yuki Iwanaga <yuki@creasty.com>
2018-01-30 17:37:23 +0900

    Configure rspec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,9 @@
+$LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', 'lib')
+
+require 'rspec'
+require 'i18n_flow'
+
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+
+RSpec.configure do |config|
+end
