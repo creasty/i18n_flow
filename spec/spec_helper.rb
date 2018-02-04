da86de15231608a5ae696a409f18460ecd71d6cd
Yuki Iwanaga <yuki@creasty.com>
2018-02-05 02:00:00 +0900

    Introduce fakefs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,12 @@
 $LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', 'lib')
 
-require 'rspec'
 require 'i18n_flow'
+require 'rspec'
+require 'fakefs/spec_helpers'
 
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
 RSpec.configure do |config|
+  config.include FakeFS::SpecHelpers
   config.include UtilMacro
 end
