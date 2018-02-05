635629f3415fd1c14219771bc15c9b24fcc8abfd
Yuki Iwanaga <yuki@creasty.com>
2018-02-05 20:19:53 +0900

    Add pry

--- i18n_flow.gemspec
+++ i18n_flow.gemspec
@@ -31,4 +31,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'rake', '~> 10.0'
   spec.add_development_dependency 'rspec', '~> 3.7'
   spec.add_development_dependency 'fakefs'
+  spec.add_development_dependency 'pry'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,8 +3,12 @@ $LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', 'lib')
 require 'i18n_flow'
 require 'rspec'
 require 'fakefs/spec_helpers'
+require 'pry'
 
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+
+Pry.config.history.should_load = false
+Pry.config.history.should_save = false
 
 RSpec.configure do |config|
   config.include FakeFS::SpecHelpers
