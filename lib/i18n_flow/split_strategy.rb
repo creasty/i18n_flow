21c01be3ef040eabf36e813c3dc9a811ce94d9cb
Yuki Iwanaga <yuki@creasty.com>
2018-02-07 15:30:30 +0900

    Fix

--- lib/i18n_flow/split_strategy.rb
+++ lib/i18n_flow/split_strategy.rb
@@ -1,3 +1,6 @@
+require 'psych'
+require_relative 'util'
+
 class I18nFlow::SplitStrategy
   DEFAULT_MAX_LEVEL      = 3
   DEFAULT_LINE_THRESHOLD = 50
@@ -18,17 +21,37 @@ class I18nFlow::SplitStrategy
     chunks
   end
 
+  def splitted_trees
+    split.map do |file_scopes, file_chunks|
+      file_path = I18nFlow::Util.scope_to_filepath(file_scopes)
+      [file_path, build_yaml(file_chunks)]
+    end.to_h
+  end
+
   def chunks
     @chunks ||= Hash.new { |h, k| h[k] = [] }
   end
 
 private
 
+  def build_yaml(file_chunks)
+    Psych::Nodes::Stream.new.tap do |stream|
+      stream.children << Psych::Nodes::Document.new do |doc|
+        doc.children << Psych::Nodes::Mapping.new do |mapping|
+          file_chunks.each do |node|
+            mapping.children << Psych::Nodes::Scalar.new(node.value)
+            mapping.children << node.psych_node
+          end
+        end
+      end
+    end
+  end
+
   def traverse(node, level:)
     content_nodes = node.content.values
 
     if level > 0
-      value_nodes, content_nodes = content_nodes.partition(&:value?)
+      content_nodes, value_nodes = content_nodes.partition(&:mapping?)
       value_nodes.each do |n|
         add_chunk(n, delta_level: -2)
       end
@@ -54,6 +77,6 @@ private
   def add_chunk(node, delta_level: 0)
     level = [node.scopes.size + delta_level, 1].max
     file_scopes = node.scopes[0...level]
-    chunks[file_scopes] << [node.start_line, node.end_line]
+    chunks[file_scopes] << node
   end
 end
--- lib/i18n_flow/yaml_ast_proxy.rb
+++ lib/i18n_flow/yaml_ast_proxy.rb
@@ -1,33 +1,18 @@
 require 'psych'
 
-class I18nFlow::YamlAstProxy
-  extend Forwardable
-
-  attr_reader :node
-  attr_reader :parent
-  attr_reader :scopes
-
-  def_delegators :node, :value
-  def_delegators :indexed_object, :each
-
-  def initialize(node, parent: nil, scopes: [])
-    @node   = node
-    @parent = parent
-    @scopes = scopes
-  end
-
+module I18nFlow::YamlAstProxy
   def self.create(node, parent: nil, scopes: [])
     case node
-    when I18nFlow::YamlAstProxy
+    when I18nFlow::YamlAstProxy::Node
       node
     when Psych::Nodes::Stream, Psych::Nodes::Document
-      new(node.children.first, parent: node, scopes: scopes)
+      Node.new(node.children.first, parent: node, scopes: scopes)
     when Psych::Nodes::Mapping
       Mapping.new(node, parent: parent, scopes: scopes)
     when Psych::Nodes::Sequence
       Sequence.new(node, parent: parent, scopes: scopes)
     else
-      I18nFlow::YamlAstProxy.new(node, parent: parent, scopes: scopes)
+      Node.new(node, parent: parent, scopes: scopes)
     end
   end
 
@@ -39,123 +24,144 @@ class I18nFlow::YamlAstProxy
     create(stream)
   end
 
-  def get(key)
-    wrap(indexed_object[key], key: key)
-  end
-  alias [] get
+  class Node
+    extend Forwardable
 
-  def set(key, value)
-    indexed_object[key] = value
-  end
-  alias []= set
+    attr_reader :node
+    attr_reader :parent
+    attr_reader :scopes
 
-  def enumerable?
-    case @node
-    when Psych::Nodes::Stream,
-      Psych::Nodes::Document,
-      Psych::Nodes::Mapping,
-      Psych::Nodes::Sequence
-      true
-    else
-      false
+    def_delegators :indexed_object, :each
+
+    def initialize(node, parent: nil, scopes: [])
+      @node   = node
+      @parent = parent
+      @scopes = scopes
+    end
+
+    def get(key)
+      wrap(indexed_object[key], key: key)
+    end
+    alias [] get
+
+    def set(key, value)
+      indexed_object[key] = value
+    end
+    alias []= set
+
+    def enumerable?
+      case @node
+      when Psych::Nodes::Stream,
+        Psych::Nodes::Document,
+        Psych::Nodes::Mapping,
+        Psych::Nodes::Sequence
+        true
+      else
+        false
+      end
+    end
+
+    def sequence?
+      @node.is_a?(Psych::Nodes::Sequence)
+    end
+
+    def mapping?
+      @node.is_a?(Psych::Nodes::Sequence)
+    end
+
+    def scalar?
+      @node.is_a?(Psych::Nodes::Scalar)
+    end
+
+    def value
+      @node.value if @node.respond_to?(:value)
+    end
+
+  private
+
+    def indexed_object
+      @indexed_object ||= I18nFlow::YamlAstProxy.create(@node, parent: @parent, scopes: scopes)
+    end
+
+    def wrap(value, key:)
+      I18nFlow::YamlAstProxy.create(value, parent: @node, scopes: [*@scopes, key])
     end
   end
 
-  def sequence?
-    @node.is_a?(Psych::Nodes::Sequence)
-  end
+  class Mapping < Node
+    extend Forwardable
 
-  def mapping?
-    @node.is_a?(Psych::Nodes::Sequence)
-  end
+    def_delegators :indexed_object, :==
 
-  def scalar?
-    @node.is_a?(Psych::Nodes::Scalar)
-  end
-
-  def value
-    @node.value if @node.respond_to?(:value)
-  end
-
-private
-
-  def indexed_object
-    @indexed_object ||= self.class.create(@node, parent: @parent, scopes: scopes)
-  end
-
-  def wrap(value, key:)
-    self.class.create(value, parent: @node, scopes: [*@scopes, key])
-  end
-end
-
-class I18nFlow::YamlAstProxy::Mapping < I18nFlow::YamlAstProxy
-  extend Forwardable
-
-  def_delegators :indexed_object, :==
-
-  def each
-    indexed_object.each do |k, _|
-      yield k, cache[k]
+    def each
+      indexed_object.each do |k, _|
+        yield k, cache[k]
+      end
     end
-  end
 
-  def keys
-    indexed_object.keys
-  end
+    def keys
+      indexed_object.keys
+    end
 
-  def values
-    indexed_object.map { |k, _| cache[k] }
-  end
+    def values
+      indexed_object.map { |k, _| cache[k] }
+    end
 
-  def set(key, value)
-    super.tap do
-      cache.delete(key)
+    def set(key, value)
+      super.tap do
+        cache.delete(key)
+        synchronize!
+      end
+    end
+    alias []= set
+
+    def batch
+      @locked = true
+      yield
+    ensure
+      @locked = false
       synchronize!
     end
-  end
-  alias []= set
 
-  def batch
-    @locked = true
-    yield
-  ensure
-    @locked = false
-    synchronize!
-  end
+  private
 
-private
+    def cache
+      @cache ||= Hash.new { |h, k| h[k] = wrap(indexed_object[k], key: k) }
+    end
 
-  def cache
-    @cache ||= Hash.new { |h, k| h[k] = wrap(indexed_object[k], key: k) }
-  end
+    def indexed_object
+      @indexed_object ||= @node.children
+        .each_slice(2)
+        .reject { |k, _| should_ignore?(k) }
+        .map { |k, v| [k.value, v] }
+        .to_h
+    end
 
-  def indexed_object
-    @indexed_object ||= @node.children
-      .each_slice(2)
-      .map { |k, v| [k.value, v] }
-      .to_h
-  end
+    def synchronize!
+      return if @locked
 
-  def synchronize!
-    return if @locked
+      children = indexed_object.flat_map { |k, v| [Psych::Nodes::Scalar.new(k), v] }
+      @node.children.replace(children)
+    end
 
-    children = indexed_object.flat_map { |k, v| [Psych::Nodes::Scalar.new(k), v] }
-    @node.children.replace(children)
-  end
-end
-
-class I18nFlow::YamlAstProxy::Sequence < I18nFlow::YamlAstProxy
-  def_delegators :indexed_object, :==, :<<
-
-  def each
-    indexed_object.each.with_index do |o, i|
-      yield i, wrap(o, key: i)
+    def should_ignore?(k)
+      k.value == '<<' && k.tag != 'tag:yaml.org,2002:str'
     end
   end
 
-private
+  class Sequence < Node
+    def_delegators :indexed_object, :==, :<<
 
-  def indexed_object
-    @node.children
+    def each
+      indexed_object.each.with_index do |o, i|
+        yield i, wrap(o, key: i)
+      end
+    end
+
+  private
+
+    def indexed_object
+      @node.children
+    end
   end
 end
