515775a6521ba22d4f19c5406a075732fa5b5af8
Yuki Iwanaga <yuki@creasty.com>
2018-02-12 22:23:40 +0900

    Add shortcut

--- lib/i18n_flow.rb
+++ lib/i18n_flow.rb
@@ -1,5 +1,17 @@
 module I18nFlow
+  extend self
+
+  def config
+    @config ||= Configuration.new.tap do |c|
+      c.auto_configure!
+    end
+  end
+
+  def configure(&block)
+    config.update(&block)
+  end
 end
 
 require_relative 'i18n_flow/version'
+require_relative 'i18n_flow/configuration'
 require_relative 'i18n_flow/validator'
---
+++ spec/lib/i18n_flow_spec.rb
@@ -0,0 +1,25 @@
+require 'i18n_flow/configuration'
+
+describe I18nFlow do
+  before do
+    allow_any_instance_of(I18nFlow::Configuration).to receive(:auto_configure!)
+  end
+
+  describe '.config' do
+    it 'should return an instance of Configuration' do
+      expect {
+        expect(I18nFlow.config).to be_a(I18nFlow::Configuration)
+      }.not_to raise_error
+    end
+  end
+
+  describe '.configure' do
+    it 'should change option values inside a block' do
+      expect {
+        I18nFlow.configure do |config|
+          expect(config).to be_a(I18nFlow::Configuration)
+        end
+      }.not_to raise_error
+    end
+  end
+end
